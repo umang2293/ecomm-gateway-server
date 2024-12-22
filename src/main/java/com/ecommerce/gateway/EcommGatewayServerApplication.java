640c10a51c5e0d8308ed15745e4e06677e608913
Umang Rathod <umangrathoduk@gmail.com>
2024-12-22 05:59:23 +0000

    Implementing API Gateway changes

---
+++ src/main/java/com/ecommerce/gateway/EcommGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommGatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommGatewayServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce/gateway/EcommGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommGatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
